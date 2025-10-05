eec8658ef25fa1264953b16fa594c65a46ae1df0
Goutam Vishwkarma <135199511+GoutamVishwkarma@users.noreply.github.com>
2025-10-05 23:43:20 +0530

    Create CountStringAppearance.js

---
+++ CountStringAppearance.js
@@ -0,0 +1,27 @@
+function countStringAppearance(str, subString) {
+  let count = 0;
+
+  for (let i = 0; i <= str.length - subString.length; i++) {
+    let match = true;
+
+    // compare each character in window with substring
+    for (let j = 0; j < subString.length; j++) {
+      // case-insensitive match
+      if (str[i + j].toLowerCase() !== subString[j].toLowerCase()) {
+        match = false;
+        break;
+      }
+    }
+
+    if (match) count++;
+  }
+
+  return count;
+}
+
+console.log(
+  countStringAppearance(
+    'hello I am goutam, hello I am keshav, Hello I am preetam.',
+    'hello'
+  )
+); // 👉 3
