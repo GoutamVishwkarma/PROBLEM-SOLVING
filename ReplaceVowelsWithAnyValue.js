91fc00ab67ae3e06be445597e192079ac288b4e3
Goutam Vishwkarma <135199511+GoutamVishwkarma@users.noreply.github.com>
2025-10-09 20:49:29 +0530

    Create ReplaceVowelsWithAnyValue.js

---
+++ ReplaceVowelsWithAnyValue.js
@@ -0,0 +1,42 @@
+// Replace all vowels with *
+//     - Input: "hello world" → "h*ll* w*rld".
+
+function replaceVowelsWithAnyValue(str, replaceBy) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let res = '';
+    
+    for(let i = 0; i < str.length; i++){
+        if(vowels.includes(str[i].toLowerCase())){
+            res += replaceBy;
+        } else {
+            res += str[i];
+        }
+    }
+    
+    return res;
+}
+
+function replaceVowelsWithAnyValueManual(str, replaceBy) {
+    // Create a lookup object - vowels as keys
+    const vowels = {
+        'a': true,
+        'e': true, 
+        'i': true,
+        'o': true,
+        'u': true
+    };
+    
+    let res = '';
+    
+    for(let i = 0; i < str.length; i++){
+        let char = str[i].toLowerCase();
+        // Check if char exists as a key in vowels object
+        if(vowels[char]){
+            res += replaceBy;
+        } else {
+            res += str[i];
+        }
+    }
+    
+    return res;
+}
